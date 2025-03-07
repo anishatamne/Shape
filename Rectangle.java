de475b093847176b3f184d48c792be570b1f51f5
Anisha Tamne <anishatamne@gmail.com>
2025-03-07 21:48:31 +0530

    Rectangle constructor

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+// Rectangle.java - Concrete class extending Shape
+class Rectangle extends Shape {
+    private double length, width;
+
+    // Constructor to initialize rectangle with length and width
+    public Rectangle(double length, double width) {
+        super("Rectangle");
+        this.length = length;
+        this.width = width;
+    }
+
+    
+}
