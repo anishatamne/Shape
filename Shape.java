47f2d07da7e23c01c392277a7587db2e88ea3d10
Anisha Tamne <anishatamne@gmail.com>
2025-03-07 21:03:19 +0530

    abstract class and constructor

---
+++ Shape.java
@@ -0,0 +1,9 @@
+// Shape.java - Abstract class defining the base structure for all shapes
+abstract class Shape {
+    protected String shapeName;
+
+    // Constructor to initialize shape name
+    public Shape(String shapeName) {
+        this.shapeName = shapeName;
+    }
+}
