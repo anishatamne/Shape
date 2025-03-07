ac380dff22046b1c748622085355dc34c510acd4
Anisha Tamne <anishatamne@gmail.com>
2025-03-07 21:52:37 +0530

    Square Constructor

---
+++ Square.java
@@ -0,0 +1,12 @@
+// Square.java - Concrete class extending Shape
+class Square extends Shape {
+    private double side;
+
+    // Constructor to initialize square with side length
+    public Square(double side) {
+        super("Square");
+        this.side = side;
+    }
+
+    
+}
